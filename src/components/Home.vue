ca51e86cd3a5a9932f9d06ee88d1afd625e32922
Juan Fernando Parra <jparrax17@gmail.com>
2020-07-16 17:50:45 +1200

    Add update functionality to Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,41 +1,70 @@
 <template>
   <b-container id="this-container">
 
-    <b-row v-for="component in object.data" :key="component">
+    <b-row v-for="jumbotron in object.data" :key="jumbotron">
       <b-col>
-        <b-card class="mt-3" :header="component">
-            <b-form @submit="onSubmit">
+        <b-card class="mt-3" header-bg-variant="primary" border-variant="primary">
+            <template v-slot:header>
+              <p class="mb-0 font-weight-bold">{{ jumbotron.title }}</p>
+            </template>         
+            <b-form :name="jumbotron.title" :data-ref="jumbotron.ref" @submit="onSubmit">
 
               <b-form-group
-                id="input-group-1"
-                label="Email address:"
-                label-for="input-1"
+                label="Title:"
               >
                 <b-form-input
-                  id="input-1"
-                  type="email"
-                  required
-                  placeholder="Enter email"
+                  :class="jumbotron.title"
+                  type="text"
+                  v-model="jumbotron.title"
                 ></b-form-input>
               </b-form-group>
 
-              <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-                <b-form-input
-                  id="input-2"
-                  required
-                  placeholder="Enter name"
-                ></b-form-input>
-              </b-form-group>
-
-              <b-form-group id="input-group-2" label="Text:" label-for="input-2">
+              <b-form-group 
+                label="Description:">
                 <b-form-textarea
-                  id="textarea"
-                  rows="3"
-                  max-rows="6"
-                ></b-form-textarea>
+                  :class="jumbotron.title"
+                  rows="6"
+                  v-model="jumbotron.content">
+                  </b-form-textarea>
               </b-form-group>
 
-              <b-button type="submit" variant="primary">Submit</b-button>
+              <b-form-group
+                label="Router:">
+                <b-form-input
+                  :class="jumbotron.title"
+                  type="text"
+                  v-model="jumbotron.router"
+                  disabled>
+                  </b-form-input>
+              </b-form-group>
+
+              <b-form-group
+                label="Button:"
+              >
+                <b-form-input
+                  :class="jumbotron.title"
+                  type="text"
+                  v-model="jumbotron.button"
+                ></b-form-input>
+              </b-form-group>
+
+              <b-form-group
+                label="Image:">
+                <b-form-input
+                  :class="jumbotron.title"
+                  type="text"
+                  v-model="jumbotron.img"
+                  disabled
+                ></b-form-input>
+              </b-form-group>
+
+              <b-button 
+                class="mt-4" 
+                variant="primary" 
+                :id="jumbotron.title"
+                @click="getFormElements">
+                Save
+              </b-button>
 
           </b-form>
         </b-card>
@@ -50,25 +79,64 @@
   export default {
     data() {
       return {
+        id: "",
         object:""
       }
     },
     methods: {
-    getHomeInfo() {
-      const endpoint = 'http://localhost:5000/home';
-      axios.get(endpoint)
-        .then((res) => {
-          this.object = res;
-          console.log(res);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
+      getHomeInfo() {
+        const endpoint = 'http://localhost:5000/home';
+        axios.get(endpoint)
+          .then((res) => {
+              this.id = res.data._id.$oid;
+              delete res.data["_id"];
+              this.object = res;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      },
       onSubmit(evt) {
         evt.preventDefault()
         alert(JSON.stringify(this.form))
       },
+      getFormElements() {
+        let event = window.event;
+        let tabRef = document.forms[event.target.getAttribute("id")].getAttribute("data-ref");
+        let formElements = document.forms[event.target.getAttribute("id")].getElementsByClassName(event.target.getAttribute("id"));
+        let info;
+        let i = 0;
+        const endpoint= `http://localhost:5000/home/${this.id}`;
+        let infoToChange = {
+          ref: '',
+          title: '',
+          content: '',
+          router: '',
+          button: '',
+          img: ''
+        }
+        
+        // Give values to json
+        for(info in infoToChange){
+         if(info == 'ref'){
+           infoToChange.ref = tabRef;
+         }else{
+          infoToChange[info] = formElements[i].value;
+          i++
+         }
+        }
+
+        // Send request
+        axios
+        .put(endpoint, infoToChange)
+        .then(() => {
+          window.alert(event.target.getAttribute("id") + ' Updated');
+        })
+        .catch((error) => {
+          console.error(error);
+          window.alert('There has been an error');
+        });
+      }
     },
     created() {
       this.getHomeInfo();
