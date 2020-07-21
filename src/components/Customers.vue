bc0845163598bfa1572441b56bd2dd035d693e4f
CJ Cucio <cuciocj@gmail.com>
2020-07-22 03:26:52 +1200

    customers component initial commit

---
+++ src/components/Customers.vue
@@ -0,0 +1,83 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <b-card
+          :header="title"
+          class="mt-3"
+        >
+          <b-table
+            :items="customers"
+            :fields="fields"
+            striped
+            hover
+          >
+            <template v-slot:cell(notes)="row">
+              <b-form-textarea
+                size="sm"
+                placeholder="Add notes"
+                rows="2"
+                v-model="row.item.notes"
+              >
+              </b-form-textarea>
+            </template>
+
+            <template v-slot:cell(save)="row">
+              <b-button size="lg" @click="onSave(row.item)">Save</b-button>
+            </template>
+          </b-table>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      title: 'Customers Information',
+      customers: [],
+      fields:  [
+        {key: "_id", thClass: 'd-none', tdClass: 'd-none'},
+        {key: "name"},
+        {key: "email"},
+        {key: "phone"},
+        {key: "notes"},
+        {key: "save"}
+      ]
+    };
+  },
+  methods: {
+    getCustomersInfo() {
+      const endpoint = 'http://localhost:5000/get_customers';
+      axios
+        .get(endpoint)
+        .then((res) => {
+          this.customers = res.data;
+          console.log(this.customers);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    onSave(item) {
+      console.log(JSON.stringify(item, null, 2));
+      const endpoint = 'http://localhost:5000/update_customer';
+      axios
+        .put(endpoint, item)
+        .then((res) => {
+          console.log('edit success');
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  },
+  created() {
+    this.getCustomersInfo();
+  }
+}
+</script>
